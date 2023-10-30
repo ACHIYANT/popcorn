24f1ccbd48f55841de7b9fe0b6032b83f299d309
ACHIYANT <achiyant@gmail.com>
2023-10-30 21:35:29 +0530

    Necessary changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+// import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import RatingNew from "./RatingNew";
+// import StarRating from "./StarRating";
+// import Rating from './Rating';
+// function Test() {
+//   const [rating, setRating] = useState(0);
+//   function handleSetRating(rate) {
+//     setRating(rate);
+//   }
+//   return (
+//     <div>
+//       <StarRating maxRating={10} color="blue" onSetRating={handleSetRating} />
+//       <p>Hi this movie got the {rating} stars.</p>
+//     </div>
+//   );
+// }
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    {/* <RatingNew maxRating={5} size={48} color={'red'} textColor={'yellow'} defaultRating={3} messages={['a','b','c','d','e']} className="rating"/> */}
+    {/* <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      color="red"
+      size={90}
+      className="test"
+      messages={["Terrible 😥", "Bad 😔", "Okay 😟", "Good 🙂", "Amazing 😊"]}
+      defaultRating={3}
+    />
+    <Rating maxRating={7} color={'green'} size={180} messages={['a','b','c','d','e','f','g']} defaultRating={5} textColor="black"/> */}
+    {/* <Test /> */}
+   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
