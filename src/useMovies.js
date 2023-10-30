6a9dbbd107fa82be98b885f61336966b6677974a
ACHIYANT <achiyant@gmail.com>
2023-10-30 21:42:57 +0530

    changes in fetch url

--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query, callback) {
           setIsLoading(true);
           setError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal }
           );
           if (!res.ok) {
